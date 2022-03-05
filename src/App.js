b0b7818f14fe1ce523027aba2851f08f7f4cb84b
muhesh <vmspride7@gmail.com>
2022-03-05 13:53:26 +0530

    fix: modal

--- src/App.js
+++ src/App.js
@@ -4,12 +4,28 @@ import { LastPage } from './container/LastPage';
 import './App.css';
 
 import Home from './Modal/Modal'
+import {Modal1} from './Modal/Modal'
 import { Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import {Button} from "react-bootstrap";
+import {useState} from "react";
 
 const Profile = () => {
   return <div>Profile</div>
+}
+const Home = () => {
+  const [modalShow, setModalShow] = useState(false);
+  
+  return (
+    <div style={{height:"100vh"}}>
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Launch modal with grid
+      </Button>
+
+      <Modal1 style={{ zIndex:"6"}} show={modalShow} onHide={() => setModalShow(false)} />
+    </div>
+  );
 }
 const Item = () => {
   return <div>Item</div>
