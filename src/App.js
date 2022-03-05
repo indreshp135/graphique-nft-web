99631c994c3310303233d8024ecf0d815df1735e
muhesh <vmspride7@gmail.com>
2022-03-05 17:07:16 +0530

    remove home-test-page

--- src/App.js
+++ src/App.js
@@ -2,30 +2,17 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { LastPage } from './container/LastPage';
 import './App.css';
-import {Modal1} from './Modal/Modal';
-import {PaymentModal} from './Modal/PaymentModal'
 import { Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import {Button} from "react-bootstrap";
-import {useState} from "react";
 
 const Profile = () => {
   return <div>Profile</div>
 }
 const Home = () => {
-  const [modalShow, setModalShow] = useState(false);
-  
-  return (
-    <div style={{height:"100vh"}}>
-      <Button variant="primary" onClick={() => setModalShow(true)}>
-        Launch modal with grid
-      </Button>
-
-      <PaymentModal dark={true} show={modalShow} onHide={() => setModalShow(false)} />
-    </div>
-  );
+  return <div>Home</div>
 }
+
 const Item = () => {
   return <div>Item</div>
 }
