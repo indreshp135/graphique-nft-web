869cca9c1f12e366ff51c89c5c833146e52467b4
indreshp135 <indreshp135@gmail.com>
2022-03-05 21:46:35 +0530

    items

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { LastPage } from './container/LastPage';
-import { HomePage } from './container/HomePage';
-import './App.css';
-import { Route, Switch } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import {ItemPage} from './ItemPage/ItemPage'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { LastPage } from "./container/LastPage";
+import { HomePage } from "./container/HomePage";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { ItemPage } from "./ItemPage/ItemPage";
 
 // import img from './images/abc.png';
 // import user1 from './images/user1.svg';
 
 const Profile = () => {
-  return <div>Profile</div>
-}
+  return <div>Profile</div>;
+};
 
 function App() {
-
   const dark = true;
 
   return (
     <div className={dark ? "dark" : "light"}>
-      <Header dark={dark}/>
-      
+      <Header dark={dark} />
+
       <Switch>
-      <Route path="/" exact component={ItemPage} />
-        <Route path="/create" exact component={Home } />
+        <Route path="/" exact component={ItemPage} />
+        <Route path="/create" exact component={LastPage} />
         <Route path="/profile" exact component={Profile} />
-        <Route path="/item" exact component={Item} />
-        
-        </Switch>
-      <Footer dark={dark}/>
+        <Route path="/item" exact component={HomePage} />
+      </Switch>
+      <Footer dark={dark} />
     </div>
   );
 }
--- src/ItemPage/ItemPage.jsx
+++ src/ItemPage/ItemPage.jsx
@@ -4,70 +4,88 @@ import image from "../lucas.png";
 import girl from "../girl.png";
 
 export const ItemPage = () => {
+  return (
+    <Container>
+      <Row xxl={12}>
+        <Col  className="d-flex justify-content-evenly my-5 align-items-center">
+          <Image
+            width="575px"
+            fluid
+            className="br-20 width-575px"
+            style={{borderRadius: "20px", maxWidth:'350px'}}
+            src={image}
+          />
+        </Col>
+        <Col  className="br-20 my-5 ">
+          <p className={classes.textTitle}>Abstact Smoke Red Blue</p>
+          <p className={classes.textSub}>
+            From &nbsp;
+            <span className={classes.textSubBold}>4.5 ETH &nbsp;</span>
+            20 of 25 available
+          </p>
+          <p className={classes.textMini}>Creator</p>
 
-    return (
-        <Container>
-            <Row>
-            <Col className='d-flex justify-content-evenly'>
-                <Image width="575px" fluid className='m-5 br-20 width-575px' src={image}/>
-            </Col>
-            <Col className='m-5 br-20 '>
-                <p className={classes.textTitle}>Abstact Smoke Red Blue</p>
-                <p className={classes.textSub}>From &nbsp;
-                <span className={classes.textSubBold}>4.5 ETH &nbsp;</span>
-                20 of 25 available
-                </p>
-                <p className={classes.textMini}>Creator</p>
-                
-                <div style={{
-                display: "flex"}}>
-            <Image className={classes.girl} src={girl}/>
-                <Container style={{
+          <div
+            style={{
+              display: "flex",
+            }}
+          >
+            <Image className={classes.girl} src={girl} />
+            <Container
+              style={{
                 display: "flex",
-                flexDirection:"column",
-                textAlign:"center"
-                }}>
-                    <p className={classes.textSubBold}>
-                        Mia Ayana</p>
-                    </Container>
-                </div>
-             
-                <div style={{
-                display: "flex",
-                flexDirection:"row",
-                flexFlow:"nowrap",
-                gap: "25px",
-                marginTop:"26px",
-                }}>
-               <p className={classes.p} style={{fontWeight:"600"}}>Details</p> 
-               <p className={classes.p}>Offers</p> 
-               <p className={classes.p}>History</p> 
-         
-            </div>
-            <div style={{
-          display: "flex",
-          flexDirection:"row",
-          marginBottom:"30px"
-          }}>
-            <div className={classes.line}/>
-            <div className={classes.lineSelect}/>
-            </div>
-            <p className={classes.p}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>  
-            <div style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          padding:"30px"
-        }}>
-         <Button 
-            className={classes.buttonConfirm}
-            > Buy for 4.5 ETH</Button>    
-         <Button className={classes.buttonCancel}
-         >Cancel</Button>  
-            </div>
-            </Col>
+                flexDirection: "column",
+                textAlign: "center",
+              }}
+            >
+              <p className={classes.textSubBold}>Mia Ayana</p>
+            </Container>
+          </div>
 
-            </Row>
-        </Container>
-    )
-}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              flexFlow: "nowrap",
+              gap: "25px",
+              marginTop: "26px",
+            }}
+          >
+            <p className={classes.p} style={{ fontWeight: "600" }}>
+              Details
+            </p>
+            <p className={classes.p}>Offers</p>
+            <p className={classes.p}>History</p>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              marginBottom: "30px",
+            }}
+          >
+            <div className={classes.line} />
+            <div className={classes.lineSelect} />
+          </div>
+          <p className={classes.p}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book
+          </p>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "30px",
+            }}
+          >
+            <Button className={classes.buttonConfirm}> Buy for 4.5 ETH</Button>
+            <button className={`${classes.buttonCancel} btn-btn`}>Cancel</button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
