6aea43be84a73ee92d59f180677ac28cbb877645
Madhavan B <bmd0219@gmail.com>
2022-03-05 17:21:35 +0530

    modular PhotoCard component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { LastPage } from './container/LastPage';
 import './App.css';
+import { PhotoCard } from './components/Cards/PhotoCard';
+import { UserCard } from './components/Cards/UserCard';
 import { Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import {Button} from "react-bootstrap";
+import {useState} from "react";
+
+import img from './images/abc.png';
 
 const Profile = () => {
   return <div>Profile</div>
@@ -17,13 +23,37 @@ const Item = () => {
   return <div>Item</div>
 }
 
+var cards = [
+  {
+    photo: img,
+    name: "Name1",
+    price: "0.1",
+    like: true,
+    likes: "100"
+  },
+  // {
+  //   photo: img,
+  //   name: "Name2",
+  //   price: "0.2",
+  //   like: false,
+  //   likes: "200"
+  // },
+  // {
+  //   photo: img,
+  //   name: "Name3",
+  //   price: "0.3",
+  //   like: true,
+  //   likes: "300"
+  // },
+]
+
 function App() {
 
   const dark = true;
 
   return (
     <div className={dark ? "dark" : "light"}>
-      <Header dark={dark}/>
+      {/* <Header dark={dark}/> */}
       
       <Switch>
       <Route path="/" exact component={LastPage} />
@@ -31,7 +61,14 @@ function App() {
         <Route path="/profile" exact component={Profile} />
         
         </Switch>
-      <Footer dark={dark}/>
+      {/* <Footer dark={dark}/> */}
+      {cards.map(card => {
+        return (
+          <PhotoCard {...card}/>
+        )
+      })}
+      {/* <br /> */}
+      <UserCard/>
     </div>
   );
 }
---
+++ src/components/Cards/PhotoCard.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Card, Container, Row, Col, Image } from "react-bootstrap";
+import classes from './PhotoCard.module.css';
+import heart from '../../images/heart.svg';
+import heartFilled from '../../images/heartfilled.svg';
+
+export const PhotoCard = (props) => {
+  return (
+    <Card className={classes.photoCard}>
+      <Card.Img className={classes.cardImage} variant="top" src={props.photo} />
+      <Card.Body className={classes.cardBody}>
+        <Container fluid style={{padding : 0}}>
+        <Row className={classes.cardBodyTitle}>
+        {props.name}
+        </Row>
+        <Row className={classes.cardBodyDesc}>
+          <Col style={{padding: 0}}>
+          <b>{props.price}</b>ETH
+          </Col>
+          <Col className={classes.cardBodyLikes}>
+            <span style={{padding: 2}}>
+            {props.like ? <Image src={heartFilled}/> : <Image src={heart}/>}
+            </span>
+            <span className={classes.cardBodyLikes}>{props.likes}</span>
+          </Col>
+        </Row>
+        </Container>
+      </Card.Body>
+    </Card>
+  );
+}
